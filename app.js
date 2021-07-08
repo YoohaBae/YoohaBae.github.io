72c8e2d5643fd8b9c4fb8261a6896edf942e77d7
YoohaBae <yooha.bae.2@gmail.com>
2021-07-08 21:03:31 +0900

    landing page finished

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/images', express.static('public'));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -35,5 +35,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
