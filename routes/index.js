76e3d38f6296378c86a5a62817881a60cdbc1668
YoohaBae <yooha.bae.2@gmail.com>
2021-07-08 21:16:36 +0900

    landing page error fixed

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Yooha Bae' });
 });
-
-
-
 module.exports = router;
