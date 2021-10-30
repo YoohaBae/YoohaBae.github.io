12370e697b391dadd07b16cd90dd28150758e7a9
YoohaBae <yooha.bae.2@gmail.com>
2021-10-31 00:56:55 +0900

    fixed json

--- routes/timeline.js
+++ routes/timeline.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+const fs = require('fs');
 
+let data= fs.readFileSync(path.resolve(__dirname, './database/projects.json'))
+console.log(data);
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('timeline', { title: 'Timeline' });
