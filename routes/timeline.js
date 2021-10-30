352b296f4a88d7a795ceadcf28e91e724a33a865
YoohaBae <yooha.bae.2@gmail.com>
2021-10-31 01:00:12 +0900

    fixed json error2

--- routes/timeline.js
+++ routes/timeline.js
@@ -3,8 +3,6 @@ var path = require('path');
 var router = express.Router();
 const fs = require('fs');
 
-let data= fs.readFileSync(path.resolve(__dirname, './database/projects.json'))
-console.log(data);
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('timeline', { title: 'Timeline' });
