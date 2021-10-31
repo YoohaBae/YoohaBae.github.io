b28040fe2d3bc19dc8cef2e4fb4d616b5cc2bce7
YoohaBae <yooha.bae.2@gmail.com>
2021-10-31 23:31:36 +0900

    filter finished

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('projects', { title: 'Projects' });
+    res.render('projects', { title: 'Projects' , filter: req.query.filter});
 });
+
+router.get('/designer', function(req, res) {
+    res.redirect(url.format({
+            pathname: "/projects",
+            query: {
+                "filter": "designer"
+            }
+        }))
+})
+router.get('/leader', function(req, res) {
+    res.redirect(url.format({
+        pathname: "/projects",
+        query: {
+            "filter": "leader"
+        }
+    }))
+})
+router.get('/developer', function(req, res) {
+    res.redirect(url.format({
+        pathname: "/projects",
+        query: {
+            "filter": "developer"
+        }
+    }))
+})
+
 module.exports = router;
